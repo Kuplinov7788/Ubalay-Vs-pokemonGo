3d262f6e4708443e18199e903bc2ccb3fec7c13c
Kuplinov7788 <emirhan.7788.8899@gmail.com>
2025-10-21 20:11:29 +0500

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1>he</h1>
+      </header>
+    </>
+  );
 };
 
 export default App;

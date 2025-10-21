6825641f1d177d1f571c2b40c715afa2b9408f85
Ubay201202 <ubaydullohhikmatx6@gmail.com>
2025-10-21 20:19:04 +0500

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Heeader from './components/Heeader'
+import Ubaydulloh from './components/Ubaydulloh'
+import Sokinma from './components/Sokinma'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-
-    // jonka
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" /> 
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Heeader />
+      <Ubaydulloh />
+      <Sokinma />
+    </div>
   )
 }
 

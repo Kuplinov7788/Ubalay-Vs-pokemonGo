14d181715c2013419674a4ebb431cbdf7f71be7e
forger6969 <saidazim186@gmail.com>
2025-10-22 17:57:16 +0500

    final cloe and pelican

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./components/Header";
 import DiscoverNow from "./components/DiscoverNow";
 import Advantages from "./components/Advantages";
+import Cloe from "./components/Cloe";
+import Pelican from "./components/Pelican";
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Header />
       <DiscoverNow />
       <Advantages />
+      <Cloe />
+      <Pelican />
     </>
   );
 };

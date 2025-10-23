69ac1a865345041341716ca74f1b154f10a0d387
Kuplinov7788 <emirhan.7788.8899@gmail.com>
2025-10-23 11:46:42 +0500

    asd

--- src/components/LogosComp.jsx
+++ src/components/LogosComp.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import logos from '../assets/logos.png'
+import React from "react";
+import logos from "../assets/logos.png";
 
 const LogosComp = () => {
-    return (
-        <div>
+  return (
+    <div>
+      <p
+        style={{ fontFamily: "cardo", fontWeight: 400 }}
+        className="text-[20px] text-center text-[#454545] pt-[120px]"
+      >
+        Those who are talking about us...
+      </p>
 
-            <p style={{ fontFamily: 'cardo', fontWeight: 400 }} className='text-[20px] text-center text-[#454545] pt-[120px]'>Those who are talking about us...</p>
+      <img className="object-cover w-[100%] pt-[30px]" src={logos} alt="" />
+    </div>
+  );
+};
 
-            <img className='object-cover w-[100%] pt-[30px]' src={logos} alt="" />
-        </div>
-    )
-}
-
-export default LogosComp
+export default LogosComp;
--- src/components/Pelican.jsx
+++ src/components/Pelican.jsx
@@ -4,7 +4,6 @@ import left from "../assets/satoru.png";
 const Pelican = () => {
   return (
     <div>
-      {/* jonka */}
       <section>
         <div className="main-wrapper flex gap-[50px] justify-start items-center pt-[150px]">
           <div className="main-right">
